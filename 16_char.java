897f0c722a1e8df67ad34e3b319b1cf4793e323e
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-09-27 21:45:06 +0530

    :one: :six: To check whether alphabet or not

--- 16_char.java
+++ 16_char.java
@@ -1,5 +1,12 @@
 public class Alphabet {
 
-public static void main(String args[]) {
-   if( (c >= 'a' && c <='z') || (c >= 'A' && c <= 'Z')) {
-     
+    public static void main(String[] args) {
+
+        char c = '*';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
